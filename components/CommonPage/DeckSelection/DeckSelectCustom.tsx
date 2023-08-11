bc4c7ba0c0f59c527840481aa20c9b5ee402e0ed
mulumulu <machinelaerning.man.ro@gmail.com>
2023-08-11 09:13:14 +0800

    m

--- components/CommonPage/DeckSelection/DeckSelectCustom.tsx
+++ components/CommonPage/DeckSelection/DeckSelectCustom.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Layout, MainContainer } from '@/Layout'
+import { MySelect } from '@/Basics'
+import { DeckListButton } from './'
+import { ReceivedDeck } from '@/MyLib/UtilsAPITyping'
 
 export default function DeckSelectCustom() {
+    const [deckList, setDeckList] = useState<ReceivedDeck[]>([])
+    const [pageType, setPageType] = useState<'NewDeck' | 'YourDeck'>('YourDeck')
+
     return (
-        <div>
-            Select Custom
-        </div>
+        <Layout>
+            <MainContainer addClass='p-4'>
+                <MySelect
+                    state={pageType}
+                    setState={setPageType}
+                    optionValues={['YourDeck', 'NewDeck']}
+                    optionTexts={['Your Original Deck', 'Create New Deck']}
+                />
+                {
+                    pageType === 'NewDeck' ?
+                        deckList.length === 0 ? <p> You have not created your own decks yet. </p> :
+                            <DeckListButton deckList={deckList} /> :
+                        pageType === 'YourDeck' ?
+                            <p> Your Deck </p> :
+                            null
+                }
+            </MainContainer>
+        </Layout>
     )
 }
--- components/CommonPage/DeckSelection/SearchBox.tsx
+++ components/CommonPage/DeckSelection/SearchBox.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FormatCategory } from './'
+import { MySelect, MyTextbox } from '@/Basics'
 
-
+const searchBoxClass = 'flex flex-col items-center justify-center p-4 bg-blue-100 rounded-md'
+const buttonClass = 'btn-second rounded-md m-4 mt-8'
 interface SearchBoxProps {
     category: string
     setCategory: React.Dispatch<React.SetStateAction<string>>
@@ -22,16 +24,37 @@ export default function SearchBox(
         setLevel, }: SearchBoxProps
 
 ) {
+    const [searchType, setSearchType] = useState<'category' | 'text'>('category')
+    const [searchText, setSearchText] = useState('')
+
+    const searchOnClick = () => {
+        console.log('search')
+    }
+
     return (
-        <div>
-            <FormatCategory
-                category={category}
-                setCategory={setCategory}
-                subcategory={subcategory}
-                setSubcategory={setSubcategory}
-                level={level}
-                setLevel={setLevel}
+        <div className={searchBoxClass}>
+            <MySelect
+                state={searchType}
+                setState={setSearchType}
+                optionValues={['category', 'text']}
+                optionTexts={['Search by Category', 'Search by Text']}
             />
+            {searchType === 'category' ?
+                <FormatCategory
+                    category={category}
+                    setCategory={setCategory}
+                    subcategory={subcategory}
+                    setSubcategory={setSubcategory}
+                    level={level}
+                    setLevel={setLevel}
+                /> :
+                <MyTextbox
+                    state={searchText}
+                    setState={setSearchText}
+                />
+            }
+
+            <button className={buttonClass} onClick={searchOnClick}> Search </button>
         </div>
     )
 }
