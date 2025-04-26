e882d6fb39a01cc319896bbdbc9e9f851200a609
mulumulu <machinelearning.man.ro@gmail.com>
2025-04-26 21:50:07 +0800

    deleted unused files

--- components/MyCustomHooks/useWindowResize.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react'
-// execute function everytime the window is resized
-export default function useWindowResize(func: () => void) {
-  useEffect(() => {
-    window.addEventListener('resize', func)
-    return () => window.removeEventListener('resize', func)
-  }, [func])
-}
