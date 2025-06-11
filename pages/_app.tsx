29d61d4d64cd3866b3afa684badc799aa0d74fbf
OBros7 <pc.app.basic@gmail.com>
2025-06-11 17:26:42 +0900

    remove next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,27 @@
-import '../styles/globals.css';
-import 'react-toastify/dist/ReactToastify.css';
-import { SessionProvider } from 'next-auth/react';
-import type { Session } from 'next-auth';
-import type { AppProps } from 'next/app';
+import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import type { AppProps } from "next/app";
 // import { Global } from '@/Global';
-import { UserProvider } from '@contexts/UserContext';
-import ReactQueryProvider from '@contexts/ReactQueryProvider';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+import { UserProvider } from "@contexts/UserContext";
+import ReactQueryProvider from "@contexts/ReactQueryProvider";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 // Stripe の公開可能キーを環境変数から取得
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
 
-// AppProps を拡張して pageProps に session を追加
-interface MyAppProps extends AppProps {
-  pageProps: AppProps['pageProps'] & {
-    session?: Session;
-  };
-}
-
-export default function App({ Component, pageProps }: MyAppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <ReactQueryProvider>
-        <Elements stripe={stripePromise}>
-          {/* <Global> */}
-          <UserProvider>
-            <Component {...pageProps} />
-            {/* </Global> */}
-          </UserProvider>
-        </Elements>
-      </ReactQueryProvider>
-    </SessionProvider>
+    // <Global>
+    <ReactQueryProvider>
+      <Elements stripe={stripePromise}>
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
+      </Elements>
+    </ReactQueryProvider>
+    // </Global>
   );
 }
