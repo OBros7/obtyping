2f58e87093a828868766f9fcf52072bdf9852889
OBros7 <pc.app.basic@gmail.com>
2025-06-13 00:07:08 +0900

    update typescript and tailwind

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
   extends: ['next/core-web-vitals', 'plugin:import/recommended', 'plugin:import/warnings', 'prettier'],
-
   rules: {},
+  settings: {
+    'import/resolver': {
+      typescript: {
+        // 何も書かなければ tsconfig.json を自動検出
+      },
+    },
+  },
 }
